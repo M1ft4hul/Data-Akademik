44a888c960304b655c48e559d95f991cbfb888a3
M1ft4hul <mift724@gmail.com>
2020-09-28 11:38:07 +0800

    coding edit modal

--- application/views/siswa/siswa.php
+++ application/views/siswa/siswa.php
@@ -214,6 +214,164 @@
     <!-- modal akhir tambah data -->
 
     <!-- modal edit Data -->
+    <div class="modal fade" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><i class="mdi mdi-account-card-details"></i> Edit Data Siswa</h5>
+                </div>
+                <div class="modal-body">
+                    <form action="<?php echo base_url('siswa/edit_data') ?>" method="POST">
+                        <div class="form-row">
+                            <div class="col-md-12 mb-4">
+                                <label for="nis">NIS</label>
+                                <input type="hidden" name="id" value="<?php echo $a->id; ?>" class="form-control">
+                                <input type="text" name="nis" value="<?php echo $a->nis ?>" class="form-control" id="nis" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="nisn">NISN</label>
+                                <input type="text" name="nisn" value="<?php echo $a->nisn ?>" class="form-control" id="nisn" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="fullname">Nama Siswa</label>
+                                <input type="text" name="nama" value="<?php echo $a->nama ?>" class="form-control" id="fullname" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="fullname">Tempat Lahir</label>
+                                <input type="text" name="tempat_lahir" value="<?php echo $a->tempat_lahir ?>" class="form-control" id="fullname" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4"">
+                                        <p class=" mb-4">Jenis Kelamin</p>
+
+                                <div class="n-chk">
+                                    <label class="new-control new-radio radio-classic-danger">
+                                        <input type="radio" class="new-control-input" name="jk" value="L">
+                                        <span class="new-control-indicator"></span>Laki-laki
+                                    </label>
+                                </div>
+
+                                <div class="n-chk">
+                                    <label class="new-control new-radio radio-classic-success">
+                                        <input type="radio" class="new-control-input" name="jk" value="P">
+                                        <span class="new-control-indicator"></span>Perempuan
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="fullname">Kelas</label>
+                                <input type="text" name="kelas" value="<?php echo $a->kelas ?>" class="form-control" id="fullname" placeholder="" value="" required>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="fullname">Agama</label>
+                                <input type="text" name="agama" value="<?php echo $a->agama ?>" class="form-control" id="fullname" placeholder="" value="" required>
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="status">Status Dalam Keluarga</label>
+                                <input type="text" name="status_dalam_keluarga" value="<?php echo $a->status_dalam_keluarga ?>" class="form-control" id="status" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="anak">Anak ke :</label>
+                                <input type="text" name="anak_ke" value="<?php echo $a->anak_ke ?>" class="form-control" id="anak" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="tlp">No. Telepon</label>
+                                <input type="text" name="no_telpon" value="<?php echo $a->no_telpon ?>" class="form-control" id="tlp" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="sekolah">Asal Sekolah</label>
+                                <input type="text" name="asal_sekolah" value="<?php echo $a->asal_sekolah ?>" class="form-control" id="sekolah" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="alamat">Alamat Siswa</label>
+                                <input type="text" name="alamat_siswa" value="<?php echo $a->alamat_siswa ?>" class="form-control" id="alamat" placeholder="" value="" required>
+
+                            </div>
+
+                            <div class="col-md-6 mb-4">
+                                <label>Tanggal Lahir</label>
+                                <input type="date" name="tanggal_lahir" value="<?php echo $a->tanggal_lahir ?>" value="0000-00-00" class="form-control flatpickr flatpickr-input active" type="text" placeholder="Select Date..">
+                            </div>
+
+                            <div class="col-md-6 mb-4">
+                                <label>Tanggal Diterima di sekolah</label>
+                                <input type="date" name="tanggal_terima" value="<?php echo $a->tanggal_terima ?>" value="0000-00-00" class="form-control flatpickr flatpickr-input active" type="text" placeholder="Select Date..">
+                            </div>
+
+                            <div class="col-md-12 mb-4">
+                                <label for="ayah">Nama Ayah</label>
+                                <input type="text" name="nama_ayah" value="<?php echo $a->nama_ayah ?>" class="form-control" id="ayah" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="ibu">Nama Ibu</label>
+                                <input type="text" name="nama_ibu" value="" class="form-control" id="ibu" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="alamat_ortu">Alamat Orang Tua</label>
+                                <input type="text" name="alamat_ortu" class="form-control" id="alamat_ortu" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="hp">No telepon Orang Tua</label>
+                                <input type="text" name="tlp_ortu" class="form-control" id="hp" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="pekerjaanAyah">Pekerjaan Ayah</label>
+                                <input type="text" name="pekerjaan_ayah" class="form-control" id="pekerjaanAyah" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="pekerjaanIbu">Pekerjaan Ibu</label>
+                                <input type="text" name="pekerjaan_ibu" class="form-control" id="pekerjaanIbu" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="wali">Nama Wali</label>
+                                <input type="text" name="nama_wali" class="form-control" id="wali" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="wali">Alamat Wali</label>
+                                <input type="text" name="alamat_wali" class="form-control" id="wali" placeholder="" value="" required>
+
+                            </div>
+                            <div class="col-md-12 mb-4">
+                                <label for="nohpwali">Nomor telepon Wali</label>
+                                <input type="text" name="telp_wali" class="form-control" id="nohpwali" placeholder="" value="" required>
+
+                            </div>
+
+                            <div class="col-md-12 mb-4">
+                                <label for="pekerjaanWali">Pekerjaan wali</label>
+                                <input type="text" name="pekerjaan_wali" class="form-control" id="PekerjaanWali" placeholder="" value="" required>
+
+                            </div>
+
+                            <div class="col-md-12 mb-4">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" class="form-control" id="password" placeholder="" value="" required>
+
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-danger btn-rounded" data-dismiss="modal"><i class="mdi mdi-close"></i> Close</button>
+                            <button type="submit" class="btn btn-outline-primary btn-rounded">Submit form</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- modal akhit edit data -->
 
     <!-- modal Detail Data -->
